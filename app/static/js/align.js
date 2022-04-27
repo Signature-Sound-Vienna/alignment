b95bcf004d95640dbb58480542c137a1a3a82e97
David M. Weigl <weigl@mdw.ac.at>
2022-04-27 17:38:37 +0200

    implement marks and skip to last mark (WIP)

--- app/static/js/align.js
+++ app/static/js/align.js
@@ -1,17 +1,28 @@
 let audios =["Donau/2001.mp3","Donau/1990.mp3","Donau/1999.mp3","Donau/2011.mp3","Donau/1980.mp3","Donau/1979.mp3","Donau/1995.mp3","Donau/1998.mp3","Donau/2007.mp3","Donau/1991.mp3","Donau/1994.mp3","Donau/2009.mp3","Donau/2005.mp3","Donau/2018.mp3","Donau/2000.mp3","Donau/1996.mp3","Donau/2004.mp3","Donau/2002.mp3","Donau/1997.mp3","Donau/1988.mp3","Donau/Compilation-1980-83.mp3","Donau/1993.mp3","Donau/1987.mp3"]
 let wavesurfers = [];
+let markers = [];
 let alignmentGrids = {};
 let ref;
 let currentAudioIx= 0;
 
-function onClickRenditionName(e) { 
-  document.getElementById(`waveform${currentAudioIx}`).classList.remove("active");
-  console.log("Clicked: ", e.target.innerText);
-  console.log("ref is: ", ref);
-  console.log("Pausing current: ", currentAudioIx);
-  console.log("Current duration: ", wavesurfers[currentAudioIx].getDuration());
-  const wasPlaying = wavesurfers[currentAudioIx].isPlaying();
-  wavesurfers[currentAudioIx].pause();
+function seekToLastMark() { 
+  if(markers.length) { 
+    const currentAlignmentIx = getClosestAlignmentIx();
+    const prevMarkers = markers.filter(m => m <= currentAlignmentIx);
+    let lastMarker;
+    if(prevMarkers.length) 
+      lastMarker = prevMarkers[prevMarkers.length-1]
+    else
+      lastMarker = 0;
+    wavesurfers[currentAudioIx].seekTo(
+      getCorrespondingTime(currentAudioIx, lastMarker) / 
+      wavesurfers[currentAudioIx].getDuration()
+    )
+  }
+}
+
+function getClosestAlignmentIx() { 
+  // return alignment index closest to current playback position
   const currentTime = wavesurfers[currentAudioIx].getCurrentTime();
   let currentGrid = alignmentGrids[audios[currentAudioIx]]
   // find the nearest marker to current playback time
@@ -23,19 +34,43 @@ function onClickRenditionName(e) {
      currentTime - currentGrid[closestAlignmentIx] > 
       currentGrid[closestAlignmentIx+1])  
           closestAlignmentIx += 1;
+  return closestAlignmentIx;
+}
+
+function getCorrespondingTime(audioIx, alignmentIx) { 
+  // get time position corresponding to current position of current audio, 
+  // in the alternative audio with index audioIx
+  let grid = alignmentGrids[audios[audioIx]];
+  return grid[alignmentIx];
+}
+
+function onClickRenditionName(e) { 
+  document.getElementById(`waveform${currentAudioIx}`).classList.remove("active");
+  console.log("Clicked: ", e.target.innerText);
+  console.log("ref is: ", ref);
+  console.log("Pausing current: ", currentAudioIx);
+  console.log("Current duration: ", wavesurfers[currentAudioIx].getDuration());
+  const wasPlaying = wavesurfers[currentAudioIx].isPlaying();
+  wavesurfers[currentAudioIx].pause();
+  let closestAlignmentIx = getClosestAlignmentIx();
   // swap to new audio and alignment grid
   currentAudioIx = audios.indexOf(e.target.innerText);
   console.log("new audio ix: ", currentAudioIx);
-  currentGrid = alignmentGrids[audios[currentAudioIx]]
+  let currentGrid = alignmentGrids[audios[currentAudioIx]]
   console.log("new audio grid: ", alignmentGrids[audios[currentAudioIx]]);
   console.log("new duration: ", wavesurfers[currentAudioIx].getDuration());
   document.getElementById(`waveform${currentAudioIx}`).classList.add("active");
   // seek to new (corresponding) position 
-  let correspondingPosition = currentGrid[closestAlignmentIx];
-  let newPosition = correspondingPosition / wavesurfers[currentAudioIx].getDuration();
-  console.log("new position: ", correspondingPosition, wavesurfers[currentAudioIx].getDuration(), newPosition);
-  wavesurfers[currentAudioIx].seekTo(newPosition);
-//  if(wasPlaying)
+  transitionToLastMark = document.getElementById(`transitionType`).checked;
+  if(transitionToLastMark) { 
+    seekToLastMark();
+  } else { 
+    let correspondingPosition = currentGrid[closestAlignmentIx];
+    let newPosition = correspondingPosition / wavesurfers[currentAudioIx].getDuration();
+    console.log("new position: ", correspondingPosition, wavesurfers[currentAudioIx].getDuration(), newPosition);
+    wavesurfers[currentAudioIx].seekTo(newPosition);
+  }
+  if(wasPlaying)
     wavesurfers[currentAudioIx].play();
 }
 
@@ -48,7 +83,6 @@ function setGrids(grids) {
     .join("") + "</ul>";
   Array.from(document.getElementsByClassName("renditionName"))
     .forEach((r, ix) => {
-      console.log("loop: ", r, ix);
       r.addEventListener("click", onClickRenditionName);
     });
 }
@@ -61,9 +95,9 @@ document.addEventListener('DOMContentLoaded', () => {
     let wavesurfer = WaveSurfer.create({
       container: `#waveform${ix}`,
       waveColor: "violet",
-      progressColor: "purple"
+      progressColor: "purple",
       //backend: "MediaElement"
-     // plugins: [ WaveSurfer.markers.create({}) ]
+      plugins: [ WaveSurfer.markers.create({}) ]
     });
     wavesurfer.load(root + "wav/" + audios[ix]);
     wavesurfers.push(wavesurfer);
@@ -95,5 +129,17 @@ document.addEventListener('DOMContentLoaded', () => {
       wavesurfers[currentAudioIx].pause();
     else 
       wavesurfers[currentAudioIx].play();
-  })
+  });
+  // mark button
+  document.getElementById("mark").addEventListener('click', function(e){
+    let toMark= getClosestAlignmentIx();
+    wavesurfers.forEach((ws, wsIx) =>  {
+      markers.push(toMark);
+      const t = getCorrespondingTime(wsIx, toMark);
+      console.log("got corresponding time: ",t) 
+      ws.addMarker({time: t, color:"green"})
+    })
+  });
+  // play from last marker button
+  document.getElementById("playLastMark").addEventListener('click', () => seekToLastMark());
 })
